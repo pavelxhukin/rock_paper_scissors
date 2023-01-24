604bbd3b4cde0f9166b77060b214a5c620a4e28d
pavelxhukin <pavel_xhukin@mail.ru>
2023-01-24 07:22:45 +0300

    logic ready

---
+++ app.js
@@ -0,0 +1,43 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    if (randomNumber === 1){
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2){
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3){
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+    if(computerChoice === userChoice){
+        result = 'its a draw'
+    }else if(computerChoice === 'rock' && userChoice=== 'paper'){
+        result = 'you win!'
+    }else if(computerChoice === 'scissors' && userChoice=== 'rock'){
+        result = 'you win!'
+    }else if(computerChoice === 'paper' && userChoice=== 'scissors'){
+        result = 'you win!'
+    }else{
+        result = 'you lost!'
+    }
+    resultDisplay.innerHTML = result
+}
